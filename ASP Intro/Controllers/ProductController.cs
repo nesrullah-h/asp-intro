c89fbfcfaf61ae00d58aa1f6eddc8a7d7b26f68c
Nesrullah Hajizade <tu7lp4qp0@aztu.local>
2022-05-24 10:10:09 +0400

    first commit

---
+++ ASP Intro/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using ASP_Intro.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ASP_Intro.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index(string search)
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product
+                {
+                    Name ="Test 1",
+                    Id=1,
+                    Image= "headphone.jpg",
+                    Description="Test 1 desc",
+                    Price=100
+                },
+                new Product
+                {
+                    Name ="Test 2",
+                    Id=2,
+                    Image= "mercedes.png",
+                    Description="Test 2 desc",
+                    Price=200
+                },
+                new Product
+                {
+                    Name ="Test 3",
+                    Id=3,
+                    Image= "phone.jpg",
+                    Description="Test 3 desc",
+                    Price=300
+                },
+            };
+
+            if (!string.IsNullOrWhiteSpace(search))
+            {
+               products= products.FindAll(p =>p.Name.ToLower().Contains(search.ToLower()));
+                if (products.Count == 0)
+                    return NotFound();
+            }
+            
+            return View();
+        }
+    }
+}
